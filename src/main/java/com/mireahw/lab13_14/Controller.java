04d67a1f6ea4d8bd56815488116d846d128bcf93
VPlay <svitalik2002@gmail.com>
2021-10-21 22:17:09 +0300

    Lab13_14

--- src/main/java/com/mireahw/lab13_14/Controller.java
+++ src/main/java/com/mireahw/lab13_14/Controller.java
@@ -2,6 +2,8 @@ package com.mireahw.lab13_14;
 
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
 public class Controller {
     private boolean init = false;
@@ -10,12 +12,22 @@ public class Controller {
     @FXML
     private Canvas canvas;
 
-    private Drawer drawer = new Drawer(canvas);
+    private GraphicsContext context;
+
+    public void drawField() {
+        for (int i = 1; i<4; i++) {
+            context.strokeLine(i * 100, 0, i * 100, 400);
+            context.strokeLine(0, i * 100, 400, i * 100);
+        }
+    }
 
     @FXML
     public void canvasClicked() {
+        this.context = canvas.getGraphicsContext2D();
         if (!init) {
-
+            context.setStroke(Color.BLACK);
+            context.setLineWidth(2);
+            drawField();
         }
     }
 }
